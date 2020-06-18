b4131bcd0ca2a5fa49f8341fe8102083b4a8aeaa
Param Vir Singh Sandhu <psandh63@my.centennialcollege.ca>
2020-06-18 17:36:10 -0400

    Add project files.

---
+++ LAB4/COMP123_M2020_LAB4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP123_M2020_LAB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
